c1199cac999e1338aeca086e48c5d2f48a2ece1f
Griffin Strain <gstrain@ilstu.edu>
2018-10-13 16:06:00 -0500

    Full graphs drawn out for 3CNF, still needs to be debugged and checked for accuracy but the code is done.

--- src/Solve3CNF.java
+++ src/Solve3CNF.java
@@ -1,8 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -19,74 +16,46 @@ public class Solve3CNF {
 		if (args.length > 0) {
 			String fileName = args[0];
 
-//			File file = new File(fileName);
+			File file = new File(fileName);
 
-//			try {
-//				Scanner sc = new Scanner(file);
-//
-//				while (sc.hasNextLine()) {
-//					String line = sc.nextLine();
-			int[][] graphData = convertCNF("4 2 1 -3 -1 -2 4 2 -4 3");
-			printGraph(graphData);
-//					SolveClique.findKClique(graphData);
-//				}
+			try {
+				Scanner sc = new Scanner(file);
 
-//				sc.close();
+				while (sc.hasNextLine()) {
+					String line = sc.nextLine();
+					int[][] graphData = convertCNF(line);
+					SolveClique.findKClique(graphData);
+				}
 
-//			} catch (FileNotFoundException e) {
-//				System.out.println("File could not be found.");
-//			}
+				sc.close();
+
+			} catch (FileNotFoundException e) {
+				System.out.println("File could not be found.");
+			}
 		} else {
 			System.out.println("No file specified.");
-		}
-	}
-	
-	public static void printGraph(int[][] graph) {
-		for (int i = 0; i < graph.length; i++) {
-			for (int j = 0; j < graph[i].length; j++) {
-				System.out.print(graph[i][j]);
-			}
-			System.out.println();
 		}
 	}
 
 	public static int[][] convertCNF(String line) {
 		// set up
-		int size = Integer.parseInt(line.substring(0, 1));
-		String toParse = line.substring(2);
-		toParse = toParse.replaceAll("\\s+", "");
-		
-		System.out.println(toParse);
+		String[] lineArray = line.split(" ");
 
-		int[] array = new int[toParse.length()];
-		
-		int count = 0;
+		int size = Integer.parseInt(lineArray[0]);
+		int[] array = new int[lineArray.length - 1];
 
-		for (int i = 0; i < toParse.length(); i++) {
-			String a = toParse.substring(i, i + 1);
-
-			if (!a.equals("-")) {
-				array[i] = Integer.parseInt(a);
-			} else {
-				int val = Integer.parseInt(toParse.substring(i + 1, i + 2)) * -1;
-				array[i] = val;
-				toParse = toParse.substring(0, i) + toParse.substring(i + 1);
-				count++;
-			}
+		for (int i = 1; i < lineArray.length; i++) {
+			array[i - 1] = Integer.parseInt(lineArray[i]);
 		}
-		
-		int[] array2 = new int[array.length - count];
-		
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] != 0) {
-				array2[i] = array[i];
-			}
-		}
-		
-		array = array2;
 
 		int[][] graphData = new int[size * 2][size * 2];
 
+		// populate the diagonal
+		for (int i = 0; i < graphData.length; i++) {
+			graphData[i][i] = 1;
+		}
+
+		// populate the graph based on the information from the line
 		for (int i = 0; i < array.length; i++) {
 			int val = i / 3;
 			val = val * 3;
@@ -96,30 +65,28 @@ public class Solve3CNF {
 				if (array[j] != 0) {
 					int iIndex = 0;
 					int jIndex = 0;
-					
-					System.out.println(array[i] + " " + array[j]);
-					
+
 					// set iIndex
 					if (array[i] > 0) {
 						iIndex = array[i] - 1;
 					} else {
-						iIndex = (-(array[i])) + 3;
+						iIndex = (-(array[i])) + (size - 1);
 					}
-					
+
 					// set jIndex
 					if (array[j] > 0) {
-						jIndex = array[j];
+						jIndex = array[j] - 1;
 					} else {
-						jIndex = -(array[j]) + 3;
+						jIndex = (-(array[j])) + (size - 1);
 					}
-					
-					if (!(val <= j && j < val2)) {	
+
+					if (!(val <= j && j < val2)) {
 						if (array[i] != -array[j]) {
 							graphData[iIndex][jIndex] = 1;
 						} else {
 							graphData[iIndex][jIndex] = 0;
 						}
-					} 
+					}
 				}
 			}
 		}
